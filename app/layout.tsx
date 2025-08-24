0da6957e9bdbe6b6e2d3cca62447d1bd17072937
dm100usa-oss <dm100usa@gmail.com>
2025-08-24 11:18:59 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className="bg-gray-50 text-gray-900">{children}</body>
+      <body className="bg-gray-50 text-gray-900">
+        {children}
+      </body>
     </html>
   );
 }
